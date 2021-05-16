83cd44437a222e0e0ca51882e6342681261ce17c
Tharindu Lakshitha <tharindulakshitha@arimaclanka.com>
2021-05-16 19:34:08 +0530

    cli implmented

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+#!/usr/bin/env node
+const program = require('commander');
+
 const filesMgmt = require('./lib/files');
 const generate = require('./lib/generate');
 
-exports.run = async function() {
+async function run() {
     console.log("Generating iSeeds...");
     const path = await filesMgmt.createDir();
     await filesMgmt.generateSeedFiles(path);
     await generate.generateSeederCode();
     console.log("iSeeds Generated Successfully");
 }
+
+
+
+program
+.version("1.0.0")
+.description("Generate Seeder files from the data in the tables. ");
+
+program
+.command('generate <tablename>')
+.description('Generate a single seeder file from the specified table')
+.action(async () => {
+    await run();
+});
+
+program.parse(process.argv);
