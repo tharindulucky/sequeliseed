26d3294fda83e1ea39de659421e9ad57025bf603
Tharindu Lakshitha <tharindulakshitha@arimaclanka.com>
2021-05-17 02:21:10 +0530

    First run successful

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ const filesMgmt = require('./lib/files');
 const query = require('./lib/database/query');
 
 async function run(table = null) {
-    console.log('\x1b[36m%s\x1b[0m',"Generating seeds...");
-    const path = await filesMgmt.createDir();
-
-    if(table !== null){
-        await query.readTable();
-    }else{
-        await query.readAllTables();
+    try{
+        console.log('\x1b[36m%s\x1b[0m',"Generating seeds...");
+        const path = await filesMgmt.createDir();
+        await query.runQueries(table);
+    }catch(err){
+        throw err;
     }
-    console.log('\x1b[32m%s\x1b[0m',"Seeds Generated Successfully!");
     
 }
 
@@ -22,13 +20,6 @@ async function run(table = null) {
 program
 .version("1.0.0")
 .description("Generate Seeder files from the data in the tables. ");
-
-program
-.command('generate:all')
-.description('Generate a single seeder file from the specified table')
-.action(async () => {
-    await run();
-});
 
 program
 .command('generate <tablenames...>')
