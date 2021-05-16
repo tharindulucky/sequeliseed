109e99810c6ad87d10e7ceb1c3600b3b7551d847
Tharindu Lakshitha <tharindulakshitha@arimaclanka.com>
2021-05-16 21:38:57 +0530

    access DB object complete

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 const program = require('commander');
 
 const filesMgmt = require('./lib/files');
-const generate = require('./lib/generate');
+const database = require('./lib/database');
 
-async function run() {
-    console.log("Generating iSeeds...");
+async function run(table = null) {
+    console.log('\x1b[36m%s\x1b[0m',"Generating seeds...");
     const path = await filesMgmt.createDir();
-    await generate.generateSeederCode();
-    console.log("iSeeds Generated Successfully!");
+
+    if(table !== null){
+        await database.readTable();
+    }else{
+        await database.readAllTables();
+    }
+    console.log('\x1b[32m%s\x1b[0m',"Seeds Generated Successfully!");
+    
 }
 
 
@@ -18,10 +24,17 @@ program
 .description("Generate Seeder files from the data in the tables. ");
 
 program
-.command('generate <tablename>')
+.command('generate:all')
 .description('Generate a single seeder file from the specified table')
 .action(async () => {
     await run();
+});
+
+program
+.command('generate <tablenames...>')
+.description('Generate a single seeder file from the specified table')
+.action(async (tablename) => {
+    await run(tablename);
 });
 
 program.parse(process.argv);
