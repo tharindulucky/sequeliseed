ae654a0e5d846b7e01d123a430122aaf6133122a
Tharindu Lakshitha <tharindulakshitha@arimaclanka.com>
2021-05-16 17:39:00 +0530

    Project setup and dependancy intallation

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-exports.printMsg = function() {
-    console.log("Hello Tharindu!");
+const filesMgmt = require('./lib/files');
+const generate = require('./lib/generate');
+
+exports.run = async function() {
+    console.log("Generating iSeeds...");
+    const path = await filesMgmt.createDir();
+    await filesMgmt.generateSeedFiles(path);
+    await generate.generateSeederCode();
+    console.log("iSeeds Generated Successfully");
 }
