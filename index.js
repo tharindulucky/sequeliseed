469dd2d6a8a2e8dee979649d1cb74e9518b1fa16
Tharindu Lakshitha <tharindulakshitha@arimaclanka.com>
2021-05-20 02:10:44 +0530

    added config options with promts

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 const program = require('commander');
+const {prompt} = require('inquirer');
 
 const filesMgmt = require('./lib/files');
 const query = require('./lib/database/query');
 const helpers = require('./lib/helpers');
+const prompts_questions = require('./lib/cli/prompts.json');
+const prompts_ans = require('./lib/cli/prompt_answers');
 
-async function run(table, options = null) {
+const config_prompts = prompts_questions['prompts']['configs'];
+
+async function run(table, options = null, answers) {
     try{
         helpers.showLog('info', "Generating seeds...");
         await filesMgmt.createDir();
@@ -13,7 +18,6 @@ async function run(table, options = null) {
     }catch(err){
         throw err;
     }
-    
 }
 
 
@@ -25,9 +29,19 @@ program
 program
 .command('generate <tablenames...>')
 .option('--env [env]', 'Specify the environement (development, test or production)')
+.option('--config [config]', 'Allows to manually enter the DB credentials and configuration.')
 .description('Generate seeder file(s) from specified table(s). Add tables seperated by a space.')
 .action(async (tablenames, options) => {
-    await run(tablenames, options);
+
+    if(options.config){
+        prompt(config_prompts).then( answers => {
+             prompts_ans.setters.setConfigs(answers);
+             run(tablenames, options);
+        });
+    }else{
+        run(tablenames, options);
+    }
+    
 });
 
 program.parse(process.argv);
